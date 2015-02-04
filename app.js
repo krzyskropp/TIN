6a39fc27d563524eefd8d5a9e9938ad684696719
Kropp <krzyskropp@gmail.com>
2015-02-04 11:02:53 +0100

    JSON and register and login

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 /* jshint node: true */
+// global $: false
+
 var app = require("express")();
 var httpServer = require("http").Server(app);
 var io = require("socket.io")(httpServer);
+var done = false;
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('client-sessions');
+
+    var Schema = mongoose.Schema;
+    var ObjectId = Schema.ObjectId;
+
+    //mongoose user model
+    var User = mongoose.model('User', new Schema({
+        id: ObjectId,
+        firstName: String,
+        lastName: String,
+        email: { 
+            type: String, 
+            unique: true
+        },
+        password: String,
+    }));
 
 var static = require('serve-static');
 var less = require('less-middleware');
@@ -15,41 +37,179 @@ app.use('/js/jquery.min.js', static(__dirname + '/bower_components/jquery/dist/j
 app.use('/js/jquery.min.map', static(__dirname + '/bower_components/jquery/dist/jquery.min.map'));
 app.use(static(path.join(__dirname, '/public')));
 
-/* io.sockets.on("connection", function (socket) {
-    socket.on("message", function (data) {
-        io.sockets.emit("echo", "No tak, tak – dostałem: " + data);
-    });
-    socket.on("error", function (err) {
-        console.dir(err);
-    });
-}); */
+    //connect to mongodb auth is a name of collection in MongoDB
+        mongoose.connect('mongodb://localhost/auth');
 
-app.use(multer({ dest: './uploads/',
-    rename: function (fieldname, filename) {
-    return filename+Date.now();
-  },
-        onFileUploadStart: function (file) {
-          console.log(file.originalname + ' jest wrzucany na serwer ...')
-        },
-        onFileUploadComplete: function (file) {
-          console.log(file.fieldname + ' wrzucono do  ' + file.path)
-          done=true;
-        }               
+    // middleware
+        app.use(bodyParser.urlencoded({extended : true}));
+    // configure session
+        app.use(session({           
+            cookieName: 'session',
+            secret: 'randomnametokryptsession',
+            duration: 60 * 1000, // in miliseconds end session after 1hour
+            aciveDuration: 5 * 60 * 1000,
+        }));
+
+
+        app.set('view engine', 'jade');
+        app.locals.pretty = true;
+
+var outputFilename = 'public/json/database.json';
+var file_name = "";
+var date = new Date();
+var username = "";
+
+function change(x)
+{
+    if (x<10){
+        x= "0" + x;
+        return x;
+    }
+    else{
+        return x;
+    }
+}
+
+var d = " (" + date.getFullYear() + "-" + change(date.getMonth())+ "-" + change(date.getDay())+ " " + change(date.getHours())+ ":" + change(date.getMinutes())+ ":" + change(date.getSeconds()) + ")";
+
+    fs.readFile(outputFilename, function (err, data) {
+        if (err) throw err;
+        var json = JSON.parse(data);
+           
+            app.use(multer({ dest: './uploads/' + username,                                                                          
+            rename: function (fieldname, filename) {               
+                    return filename + Date.now();
+                },
+                                                                   
+    onFileUploadStart: function (file) {
+      console.log(file.originalname + ' jest dodawany ...')
+    },
+    onFileUploadComplete: function (file, originalname, extension, size, encoding) {
+      console.log(file.originalname + ' dodano do  ' + file.path)
+      
+      var number = file.extension.length + 1;
+        
+      var file_data = {
+          "name": file.originalname.slice(0, -number)+d,
+          "originalname": file.originalname,
+          "extension": file.extension,
+          "size": Math.round((file.size/1024)) + "KB",
+          "encoding": file.encoding
+      };
+      
+      json.files.unshift(file_data);
+      
+      fs.writeFile(outputFilename,JSON.stringify(json, null, 4), function(err){
+         if (err){
+             console.log(err);
+         } 
+          else{
+              console.log("Dodano te dane?!");
+          }
+      });       
+                
+      done=true;
+    }
 }));
-
-app.get('/',function(req,res){
-      res.sendfile("./public/index.html");
+        
+app.get('',function(req,res){
+      res.render("index.jade");
 });
 
+        // render views to login
+        app.get('/register', function(req,res){
+            res.render("register.jade");
+        }); 
+        
+        app.post('/register', function(req,res){
+            var user = new User({            
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: req.body.password
+            });
+            
+            user.save(function(err) {
+                if(err) {
+                    var err = 'Błąd przy rejestracji! Spróbuj ponownie';
+                    
+                    // mongodb sprawdza czy wprowadzony email jest w bazie mongodb
+                    // email UNIQUE
+                    if (err.code === 11000){
+                        error = 'Wprowadzono już taki adres email. Wprowadz inny';
+                }
+                    res.render('register.jade', { error: error});     
+            } 
+                else{
+                        res.redirect('/dashboard');
+                        username = req.body.email;
+                       fs.mkdir('./uploads/' + username );
+                       fs.writeFile('./uploads/' + username + "/" + "database" + ".json", '{"files":[] }');
+                    }
+        });
+        });
+
+        app.get('/login', function(req,res){
+            res.render("login.jade");
+        }); 
+        
+        app.post('/login', function(req,res){
+            User.findOne({ email: req.body.email }, function (err, user){
+                if (!user){
+                    res.render('login.jade', { error: 'Invalid email or password.'});
+                }
+                else{
+                    if (req.body.password === user.password){
+                        req.session.user = user;
+                        res.render("index.jade");
+                        fs.readFile('./uploads/' + user.cookieName + "/" + "database.json")
+                    }
+                    else {
+                        res.render('login.jade', { error: 'Invalid email or password.'});
+                    }
+                }
+            });
+        }); 
+
+        app.get('/dashboard', function(req,res){
+            if (req.session && req.session.user) {
+                User.findOne({ email: req.session.user.email}, function(err, user){
+                    if (!user){
+                        req.session.reset();
+                        res.redirect('/login');
+                    }
+                    else{
+                        res.locals.user = user;
+                        res.render('dashboard.jade')
+                    }
+                });
+            }
+            else{
+                res.redirect('/login');
+            }
+            console.log(req.body);
+            res.render("dashboard.jade");
+        });
+
+        app.get('/', function(req, res){
+           res.redirect("index.jade"); 
+        });
+
+        app.get('/logout', function(req, res){
+           res.redirect("/"); 
+        });
+        
+        
+        
 app.post('',function(req,res){
-  if(done==true){
-    req.files >> "file_details.txt";
-    console.log(req.files);
-    res.redirect("back");   
-  }
+  if(done==true){     
+        console.log(req.files);
+        res.redirect("/login");
+  }     
+});         
 });
-
-
-httpServer.listen(port, function () {
-    console.log('Serwer HTTP działa na porcie ' + port);
+               
+/*Run the server.*/
+app.listen(3000,function(){
+    console.log("Serwer pracuje na porcie 3000");
 });
--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -1,60 +1,22 @@
-/* jshint browser: true, globalstrict: true, devel: true */
-/* global io: false */
-"use strict";
-
-// Inicjalizacja
-window.addEventListener("load", function (event) {
-    var status = document.getElementById("status");
-    var open = document.getElementById("open");
-    var close = document.getElementById("close");
-    var send = document.getElementById("send");
-    var text = document.getElementById("text");
-    var message = document.getElementById("message");
-    var socket;
-
-    status.textContent = "Brak połącznia";
-    close.disabled = true;
-    send.disabled = true;
-
-    // Po kliknięciu guzika „Połącz” tworzymy nowe połączenie WS
-    open.addEventListener("click", function (event) {
-        open.disabled = true;
-        if (!socket || !socket.connected) {
-            socket = io({forceNew: true});
-        }
-        socket.on('connect', function () {
-            close.disabled = false;
-            send.disabled = false;
-            status.src = "img/bullet_green.png";
-            console.log('Nawiązano połączenie przez Socket.io');
-        });
-        socket.on('disconnect', function () {
-            open.disabled = false;
-            status.src = "img/bullet_red.png";
-            console.log('Połączenie przez Socket.io zostało zakończone');
-        });
-        socket.on("error", function (err) {
-            message.textContent = "Błąd połączenia z serwerem: '" + JSON.stringify(err) + "'";
-        });
-        socket.on("echo", function (data) {
-            message.textContent = "Serwer twierdzi, że otrzymał od Ciebie: '" + data + "'";
-        });
-    });
+$.getJSON("../json/database.json", function(data){
+    var dane = data;
+    var wpis = "Brak plikow";
+    var out = "<table>" + "<tr>" + "<td>" + "Nazwa pliku" + "</td>" + "<td>" + "Rozmiar pliku" + "</td>" + "<td>" + "Rozszerzenie" + "</td>" + "</tr>";
     
-    // Zamknij połączenie po kliknięciu guzika „Rozłącz”
-    close.addEventListener("click", function (event) {
-        close.disabled = true;
-        send.disabled = true;
-        open.disabled = false;
-        message.textContent = "";
-        socket.io.disconnect();
-        console.dir(socket);
-    });
-
-    // Wyślij komunikat do serwera po naciśnięciu guzika „Wyślij”
-    send.addEventListener("click", function (event) {
-        socket.emit('message', text.value);
-        console.log('Wysłałem wiadomość: ' + text.value);
-        text.value = "";
-    });
+    if(dane["files"].length == 0) {
+        out += "<tr>" + "<td>" + wpis + "</td>" + "</tr>";
+    }
+    else{
+        for(var i=0; i<dane["files"].length; i++){        
+            out  += "<tr>" +
+            "<td>" + dane["files"][i].name + "</td>" +
+            "<td>" + dane["files"][i].size + "</td>" +
+            "<td>" + dane["files"][i].extension + "</td>" +
+            "</tr>";
+        }
+    }
+    
+     out += "</table>";
+     document.getElementById("table").innerHTML = out;
 });
+
